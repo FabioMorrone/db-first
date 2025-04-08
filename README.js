4706ea17a67c9da76e0eca499f1c588b78191483
FabioMorrone <morronefabio04@gmail.com>
2025-04-08 16:51:49 +0200

    Add initial README structure for `cars` table schema

---
+++ README.js
@@ -0,0 +1,21 @@
+
+/*
+
+## Nome tabella: `cars`
+
+
+
+- id: BIGINT – chiave primaria – auto_increment – NOT NULL  
+- modello: VARCHAR(100) – NOT NULL  
+- anno: YEAR() – NOT NULL  
+- prezzo: DECIMAL(10,2) – NOT NULL  
+- chilometraggio: INT – NULL  
+- colore: VARCHAR(50) – NULL  
+- porte: TINYINT – NULL  
+- cilindrata: SMALLINT – NULL  
+- cavalli: SMALLINT – NULL  
+- immagine_copertina: VARCHAR(255) – NULL  
+ 
+
+
+*/
